432a796eb961c546a39aa953dfa5d09266a28efb
jmundo <jmundo-31@live.com>
2014-11-09 23:29:33 -0800

    finished main framework

--- js/app.js
+++ js/app.js
@@ -3,3 +3,90 @@
     app.js, main Angular application script
     define your module and controllers here
 */
+
+var reviewUrl = 'https://api.parse.com/1/classes/reviews'
+
+
+angular.module('RatingApp', ['ui.bootstrap'])
+    .config(function($httpProvider) {
+        $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'pcmpF6Z96vo6cmdacQNVDYog38VQP8baYOIMLjPF';
+        $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'SXOU8BCJX6VClPmO3tXrceClE4T70nI8fpAtWf0y';
+    })
+
+    .controller('RatingsController', function($scope, $http){
+        $scope.refreshReviews = function () {
+            $http.get(reviewUrl)
+                .success(function (data) {
+                    $scope.reviews = data.results
+                });
+        };
+        $scope.refreshReviews();
+        $scope.newReview = {
+            rating: 0,
+            name: null,
+            title: null,
+            body: null,
+            score: 0
+
+        };
+
+        $scope.addReview = function(){
+            $scope.inserting = true;
+            $scope.showForm();
+            var review = $scope.newReview = {
+                rating: $scope.rate,
+                name: document.getElementById('reviewer-name').value,
+                title: document.getElementById('review-title').value,
+                body: document.getElementById('review').value,
+                score: 0
+            };
+            console.log($scope.rate);
+            $http.post(reviewUrl, review)
+                .success(function(responseData){
+                    $scope.newReview.objectId = responseData.objectId;
+                    $scope.reviews.push($scope.newReview);
+                })
+                .finally(function(){
+                    $scope.inserting = false;
+                    document.getElementById('reviewer-name').value = null;
+                    document.getElementById('review-title').value = null;
+                    document.getElementById('review').value = null;
+                    $scope.rate = 0;
+                });
+        };
+
+        $scope.deleteComment = function(review) {
+            $http.delete(reviewUrl + "/" + review.objectId, review)
+                .success(function(){
+                    $scope.refreshReviews();
+                });
+        };
+
+        $scope.vote = function(review, vote){
+            console.log("voted");
+            if(vote == 1){
+                $http.put(reviewUrl + "/" + review.objectId, review.score ++, review);
+            } else {
+                $http.put(reviewUrl + "/" + review.objectId, review.score --, review);
+            }
+            //vote up or down and do not update!
+        }
+
+        $scope.rate = 0;
+        $scope.max = 5;
+        $scope.isReadonly = false;
+
+        $scope.hoveringOver = function(value) {
+            $scope.overStar = value;
+            $scope.percent = 100 * (value / $scope.max);
+        };
+
+        document.getElementById('add-comment').style.display = 'none';
+        $scope.showForm = function(){
+            if(document.getElementById('add-comment').style.display == 'block') {
+                document.getElementById('add-comment').style.display = 'none';
+            } else {
+                document.getElementById('add-comment').style.display = 'block';
+            }
+        }
+    });
